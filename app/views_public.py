3bc6d0442776ca371a7364f6c1a2c263708ff24c
40340769 <40340769@live.napier.ac.uk>
2021-12-12 22:01:09 +0000

    creates base.html (for public) and home.html templates, fixes link to main.css

--- app/views_public.py
+++ app/views_public.py
@@ -4,3 +4,7 @@ import sqlite3
 
 db_location = 'app/var/strainer.db'
 views_public = Blueprint('views_public', __name__)
+
+@views_public.route('/', methods=['GET','POST'])
+def home():
+	return render_template("public/home.html")
