58450396e1e1297acb940c9955c942cc43878461
40340769 <40340769@live.napier.ac.uk>
2021-12-13 15:37:23 +0000

    creates most of auth views

--- app/views_auth.py
+++ app/views_auth.py
@@ -5,3 +5,64 @@ import bcrypt
 
 db_location = 'app/var/strainer.db'
 views_auth = Blueprint('views_auth', __name__)
+
+@views_auth.route('/logout', methods=['GET','POST'])
+@requires_login
+def logout():
+	session.pop('logged',None)
+	session.pop('email',None)
+	session.pop('user_id',None)
+	return redirect(url_for('views_public.home'))
+
+@views_auth.route('/my-bookmarks', methods=['GET','POST'])
+@requires_login
+def my_bookmarks():
+	return render_template("auth/my_bookmarks.html")
+
+@views_auth.route('/account', methods=['GET','POST'])
+@requires_login
+def account():
+	return render_template("auth/account.html")
+
+@views_auth.route('/new-bookmark', methods=['GET','POST'])
+@requires_login
+def new_bookmark():
+	conn = sqlite3.connect(db_location)
+	cursor = conn.cursor()
+
+	query_user = "SELECT * FROM users WHERE email=?"
+	cursor.execute(query_user,(session['email'],))
+	user_info = cursor.fetchall()
+	if user_info == []:
+		pass 
+	else:	
+		session['user_id'] = user_info[0][0]
+		user_id = user_info[0][0]
+	conn.close()
+
+	if request.method == 'POST':
+		name = request.form.get('name')
+		category = request.form.get('category')
+		url = request.form.get('url')
+		note = request.form.get('note')
+
+		if len(name) < 1:
+			flash('Name of the bookmark is too short!', category='error')
+		elif len(category) < 1:
+			flash('Category of the bookmark is too short!', category='error')
+		elif len(url) < 1:
+			flash('URL of the bookmark is too short!', category='error')
+		elif len(note) < 1:
+			flash('Note for the bookmark is too short!', category='error')
+		else:
+			# Add bookmark to database
+			conn = sqlite3.connect(db_location)
+			cursor = conn.cursor()
+			query = "INSERT INTO bookmarks (name,category,url,notes,user_id) VALUES (?,?,?,?,?)"
+			cursor.execute(query,(name,category,url,note,user_id))
+			conn.commit()
+			conn.close()
+			flash('Bookmark added!', category='success')
+			return redirect(url_for('views_auth.my_bookmarks'))
+
+	return render_template("auth/new_bookmark.html")
--- app/views_public.py
+++ app/views_public.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash, request, session, redirect, url_for
 from . import requires_login
 import sqlite3
+import bcrypt
 
 db_location = 'app/var/strainer.db'
 views_public = Blueprint('views_public', __name__)
@@ -15,10 +16,81 @@ def about():
 
 @views_public.route('/login', methods=['GET','POST'])
 def login():
+	# Login button has been clicked
+	if request.method == 'POST':
+		email = request.form.get('email')
+		password = request.form.get('password')
+		conn = sqlite3.connect(db_location)
+		cursor = conn.cursor()
+		# Checks if the user (email) already exists
+		query = "SELECT * FROM users WHERE email=?"
+		cursor.execute(query,(email,))
+		user = cursor.fetchall()
+		# User (email) doesn't exists
+		if user == []:
+			flash('Wrong email or password. Please try again.', category='error')
+		# User (email) exists
+		else:	
+			email_db = user[0][1]
+			password_db = user[0][4]
+			# User (email) and email match the values from the database
+			if(email_db == email and password_db == bcrypt.hashpw(password.encode('utf-8'),password_db)):
+				flash('Logged in successfully!', category='success')
+				session['logged'] = True
+				session['email'] = email
+				return redirect(url_for('views_auth.my_bookmarks'))
+			# User (email) and email don't match the values from the database
+			else:
+				flash('Wrong email or password. Please try again', category='error')
+	# Display empty Login form
 	return render_template("public/login.html")
 
 @views_public.route('/register', methods=['GET','POST'])
 def register():
+	# Register button has been clicked
+	if request.method == 'POST':
+		email = request.form.get('email')
+		first_name = request.form.get('firstName')
+		last_name = request.form.get('lastName')
+		password1 = request.form.get('password1')
+		password2 = request.form.get('password2')
+		password_hashed = bcrypt.hashpw(password1.encode('utf-8'),bcrypt.gensalt())
+		# Checks if data has enough length and if passwords match
+		if len(email) < 4:
+			flash('Email must be greater than 3 characters.', category='error')
+		elif len(first_name) < 2:
+			flash('First name must be greater than 1 character.', category='error') 
+		elif len(last_name) < 2:
+			flash('Last name must be greater than 1 character.', category='error')
+		elif len(password1) < 7:
+			flash('Password must be greater than 6 characters.', category='error')
+		elif password1 != password2:
+			flash('Passwords don\'t match.', category='error')
+		else:
+			conn = sqlite3.connect(db_location)
+			cursor = conn.cursor()
+			# Checks if the user (email) already exists
+			query = "SELECT * FROM users WHERE email=?"
+			cursor.execute(query,(email,))
+			user = cursor.fetchall()
+			# User (email) already exists
+			if user != []:
+				flash('Email already exists. Try a different email.', category='error')
+			# User (email) is available
+			else:	
+				# Add user to the database
+				conn = sqlite3.connect(db_location)
+				cursor = conn.cursor()
+				query = "INSERT INTO users (email,first_name,last_name,h_password) VALUES (?,?,?,?)"
+				cursor.execute(query,(email,first_name,last_name,password_hashed))
+				conn.commit()
+				conn.close()
+				flash('Account created successfully!', category='success')
+				session['logged'] = True
+				session['email'] = email
+				# Redirect to My Bookmarks page
+				return redirect(url_for('views_auth.my_bookmarks'))
+	# Display empty Register form
 	return render_template("public/register.html")
 
 @views_public.route('/contact', methods=['GET','POST'])
